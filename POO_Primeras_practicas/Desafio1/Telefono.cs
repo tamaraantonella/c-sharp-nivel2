513913327d864334a8cc0159052b1e040bf107a4
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-12 17:55:09 -0300

    get & set, class constructor

--- POO_Primeras_practicas/Desafio1/Telefono.cs
+++ POO_Primeras_practicas/Desafio1/Telefono.cs
@@ -13,6 +13,10 @@ namespace Desafio1
         private string numeroTelefonico;
         private int codigoOperador;
 
+        public Telefono(string modelo, string marca) {
+            this.modelo = modelo;
+            this.marca = marca;
+        }
         public string Modelo
         {
             get { return modelo; }
@@ -24,6 +28,15 @@ namespace Desafio1
         public string NumeroTelefonico { get; set; }
         public int CodigoOperador {
             get { return codigoOperador; }
+            set {
+                if (value == 1 || value == 2 || value == 3)
+                {
+                    codigoOperador = value;
+                }
+                else {
+                    codigoOperador = 0;
+                }
+            }
         }
     }
 }
