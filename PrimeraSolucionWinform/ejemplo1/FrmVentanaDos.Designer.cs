cedb68f22041ac109054a58f4bf35c7108433b30
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-17 10:57:56 -0300

    form + message Box

--- PrimeraSolucionWinform/ejemplo1/FrmVentanaDos.Designer.cs
+++ PrimeraSolucionWinform/ejemplo1/FrmVentanaDos.Designer.cs
@@ -38,6 +38,10 @@
             this.btnAddPets = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.tbPets = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.checkVet = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -45,70 +49,80 @@
             this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(29, 269);
+            this.button1.Location = new System.Drawing.Point(33, 469);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 54);
+            this.button1.Size = new System.Drawing.Size(151, 74);
             this.button1.TabIndex = 0;
             this.button1.Text = "Saludar";
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 24);
+            this.label1.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 110);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.Size = new System.Drawing.Size(120, 18);
             this.label1.TabIndex = 1;
             this.label1.Text = "Ingresa tu nombre";
             // 
             // textNombre
             // 
-            this.textNombre.Location = new System.Drawing.Point(117, 21);
+            this.textNombre.Location = new System.Drawing.Point(154, 107);
+            this.textNombre.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.textNombre.Name = "textNombre";
-            this.textNombre.Size = new System.Drawing.Size(100, 20);
+            this.textNombre.Size = new System.Drawing.Size(266, 26);
             this.textNombre.TabIndex = 2;
             // 
             // saludarLabel
             // 
             this.saludarLabel.AutoSize = true;
-            this.saludarLabel.Location = new System.Drawing.Point(288, 300);
+            this.saludarLabel.Location = new System.Drawing.Point(383, 484);
+            this.saludarLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.saludarLabel.Name = "saludarLabel";
-            this.saludarLabel.Size = new System.Drawing.Size(0, 13);
+            this.saludarLabel.Size = new System.Drawing.Size(0, 18);
             this.saludarLabel.TabIndex = 3;
             this.saludarLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // labelUbicacion
             // 
             this.labelUbicacion.AutoSize = true;
-            this.labelUbicacion.Location = new System.Drawing.Point(26, 57);
+            this.labelUbicacion.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUbicacion.Location = new System.Drawing.Point(26, 146);
+            this.labelUbicacion.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.labelUbicacion.Name = "labelUbicacion";
-            this.labelUbicacion.Size = new System.Drawing.Size(89, 13);
+            this.labelUbicacion.Size = new System.Drawing.Size(113, 18);
             this.labelUbicacion.TabIndex = 4;
             this.labelUbicacion.Text = "Ingresá tu ciudad";
             // 
             // textUbicacion
             // 
-            this.textUbicacion.Location = new System.Drawing.Point(117, 54);
+            this.textUbicacion.Location = new System.Drawing.Point(154, 143);
+            this.textUbicacion.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.textUbicacion.Name = "textUbicacion";
-            this.textUbicacion.Size = new System.Drawing.Size(100, 20);
+            this.textUbicacion.Size = new System.Drawing.Size(266, 26);
             this.textUbicacion.TabIndex = 5;
             // 
             // listViewPets
             // 
             this.listViewPets.HideSelection = false;
-            this.listViewPets.Location = new System.Drawing.Point(29, 173);
+            this.listViewPets.Location = new System.Drawing.Point(33, 336);
+            this.listViewPets.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.listViewPets.Name = "listViewPets";
-            this.listViewPets.Size = new System.Drawing.Size(193, 74);
+            this.listViewPets.Size = new System.Drawing.Size(255, 101);
             this.listViewPets.TabIndex = 6;
             this.listViewPets.UseCompatibleStateImageBehavior = false;
             this.listViewPets.View = System.Windows.Forms.View.List;
             // 
             // btnAddPets
             // 
-            this.btnAddPets.Location = new System.Drawing.Point(147, 129);
+            this.btnAddPets.Location = new System.Drawing.Point(187, 296);
+            this.btnAddPets.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.btnAddPets.Name = "btnAddPets";
-            this.btnAddPets.Size = new System.Drawing.Size(75, 23);
+            this.btnAddPets.Size = new System.Drawing.Size(101, 32);
             this.btnAddPets.TabIndex = 7;
             this.btnAddPets.Text = "Agregar";
             this.btnAddPets.UseVisualStyleBackColor = true;
@@ -117,24 +131,73 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 92);
+            this.label2.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 280);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.Size = new System.Drawing.Size(136, 18);
             this.label2.TabIndex = 8;
             this.label2.Text = "Agregá tus mascotas";
             // 
             // tbPets
             // 
-            this.tbPets.Location = new System.Drawing.Point(29, 132);
+            this.tbPets.Location = new System.Drawing.Point(33, 302);
+            this.tbPets.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.tbPets.Name = "tbPets";
-            this.tbPets.Size = new System.Drawing.Size(100, 20);
+            this.tbPets.Size = new System.Drawing.Size(132, 26);
             this.tbPets.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 182);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 50);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ingresá tu fecha de nacimiento";
+            // 
+            // fechaNacimiento
+            // 
+            this.fechaNacimiento.Location = new System.Drawing.Point(154, 182);
+            this.fechaNacimiento.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.fechaNacimiento.Name = "fechaNacimiento";
+            this.fechaNacimiento.Size = new System.Drawing.Size(266, 26);
+            this.fechaNacimiento.TabIndex = 11;
+            // 
+            // checkVet
+            // 
+            this.checkVet.AutoSize = true;
+            this.checkVet.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkVet.Location = new System.Drawing.Point(33, 238);
+            this.checkVet.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.checkVet.Name = "checkVet";
+            this.checkVet.Size = new System.Drawing.Size(217, 22);
+            this.checkVet.TabIndex = 12;
+            this.checkVet.Text = "Tenés veterinario de confianza?";
+            this.checkVet.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Location = new System.Drawing.Point(23, 40);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 33);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Perfil del usuario";
             // 
             // FrmVentanaDos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1066, 623);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkVet);
+            this.Controls.Add(this.fechaNacimiento);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.tbPets);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnAddPets);
@@ -145,8 +208,10 @@
             this.Controls.Add(this.textNombre);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "FrmVentanaDos";
-            this.Text = "Form2";
+            this.Text = "Mi perfil";
             this.Load += new System.EventHandler(this.FrmVentanaDos_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -165,5 +230,9 @@
         private System.Windows.Forms.Button btnAddPets;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox tbPets;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker fechaNacimiento;
+        private System.Windows.Forms.CheckBox checkVet;
+        private System.Windows.Forms.Label label4;
     }
 }
--- PrimeraSolucionWinform/ejemplo1/FrmVentanaDos.cs
+++ PrimeraSolucionWinform/ejemplo1/FrmVentanaDos.cs
@@ -24,19 +24,21 @@ namespace ejemplo1
 
         }
 
-
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string texto = textNombre.Text;
-            string ciudad = textUbicacion.Text;
-            saludarLabel.Text = "Hola " + texto + " que bueno que nos visites desde " + ciudad;
-        }
-
         private void btnAddPets_Click(object sender, EventArgs e)
         {
             string pet = tbPets.Text;
             listViewPets.Items.Add(pet);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string nombre = textNombre.Text;
+            string ciudad = textUbicacion.Text;
+            DateTime fecha = fechaNacimiento.Value;
+            string vet = checkVet.Checked == true ? "si" : "no";
+            string message = "Nombre " + nombre + " de la ciudad de " + ciudad + ", con fecha de nacimiento " + fecha + " y con veterinario?: " + vet;
+            MessageBox.Show(message);
+            //saludarLabel.Text = "Hola " + texto + " que bueno que nos visites desde " + ciudad;
         }
     }
 }
